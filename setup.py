f5f77560616810d5fa6aebac248df48b60f88aae
BossSolutions-Debug <venkat.kanamarlapudi1906@gmail.com>
2026-03-09 12:47:24 +0530

    created environment

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot_RAG",
+    version="0.1.0",
+    author="Anjani Kumar",
+    author_email="venkat.kanamarlapudi1906@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
